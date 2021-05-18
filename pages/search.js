2284c0d3b941c2692337bfd631a8a6c1a53d38b2
Atishay jain <69712595+atishayzn@users.noreply.github.com>
2021-05-18 20:45:43 +0530

    fix env  variables for vercel deployment

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Response from "../Response"
 import {useRouter} from "next/router"
 import SearchResults from "../components/SearchResults";
-import {API_KEY,CONTEXT_KEY} from "../.env"
+//import {API_KEY,CONTEXT_KEY} from "../.env"
 
 function Search({results}) {
     
@@ -31,7 +31,7 @@ export async function getServerSideProps(context){
     const useDummyData = false;
     const startIndex = context.query.start || "0";
 
-    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
     .then(response => response.json());
 
     //AFTER SERVER RENDERED
